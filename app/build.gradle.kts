a569f2fef35e1cd969871f15d0fb522342ee2c8c
lance.li <lingzhang.li@mhp.com>
2021-11-05 15:16:35 +0800

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("fate.android-app-default")
   id("fate.android-compose")
+  id("dagger.hilt.android.plugin")
 }
 android {
   defaultConfig {
@@ -12,4 +13,7 @@ dependencies{
   implementation(Lib.AndroidX.coreSplash)
   implementation(Lib.AndroidX.fragmentKtx)
   implementation(Lib.materialComponents)
+  implementation(Lib.Dagger.hilt)
+  kapt(Lib.Dagger.hiltCompiler)
+
 }
